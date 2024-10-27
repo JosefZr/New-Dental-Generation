b65aec28966c152061f4da571092c938e7666a42
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-10-27 16:43:28 +0100

    feat: get channel by id

--- server/routes/channels.js
+++ server/routes/channels.js
@@ -9,7 +9,7 @@ router.post("/", ChannelController.createChannel); // Create a new channel
 router.put("/:id", ChannelController.updateChannel); // Update channel name and description
 router.patch("/:id/lock", ChannelController.lockChannel); // Lock or unlock a channel
 router.delete("/:id", ChannelController.deleteChannel); // Delete a channel
-// router.get("/:id", ChannelController.getChannelById); // Get a specific channel by ID
+router.get("/:id", ChannelController.getChannelById); // Get a specific channel by ID
 router.get("/", ChannelController.getAllChannels); // Get all channels
 
 export default router;
