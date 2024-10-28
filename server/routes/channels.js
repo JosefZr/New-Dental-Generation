ce180d396045309c84e6547cc752c3231c50c267
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-10-28 16:00:29 +0100

    feat: adding middlewares to routes

--- server/routes/channels.js
+++ server/routes/channels.js
@@ -1,15 +1,31 @@
 import express from "express";
 import * as ChannelController from "../controllers/channels.controllers.js";
+import { authenticateToken } from "../middlewares/auth.js";
+import { authorizedRoles } from "../middlewares/roles.js";
 
 const router = express.Router();
 
-// TODO: middlewares (admin and moderators)
+router.use(authenticateToken);
 
-router.post("/", ChannelController.createChannel); // Create a new channel
-router.put("/:id", ChannelController.updateChannel); // Update channel name and description
-router.patch("/:id/lock", ChannelController.lockChannel); // Lock or unlock a channel
-router.delete("/:id", ChannelController.deleteChannel); // Delete a channel
 router.get("/:id", ChannelController.getChannelById); // Get a specific channel by ID
 router.get("/", ChannelController.getAllChannels); // Get all channels
+
+router.put(
+  "/:id",
+  authorizedRoles("admin", "moderator"),
+  ChannelController.updateChannel
+); // Update channel
+router.patch(
+  "/:id/lock",
+  authorizedRoles("admin", "moderator"),
+  ChannelController.lockChannel
+); // Lock/unlock channel
+
+router.post("/", authorizedRoles("admin"), ChannelController.createChannel); // Create a new channel
+router.delete(
+  "/:id",
+  authorizedRoles("admin"),
+  ChannelController.deleteChannel
+); // Delete a channel
 
 export default router;
