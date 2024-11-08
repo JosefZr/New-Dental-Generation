6877de91e4b9ccd763c7088828ad877e334ab694
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-11-08 20:09:47 +0100

    feat: stript payment and success page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
 // App.jsx
-import { Routes, Route } from 'react-router-dom';
-import { Chat, DentalLab, DentalStore, Dentist, Intro, Login, Signup } from './pages';
-import Chat1 from './pages/chat/chat1';
-import Chat2 from './pages/chat/chat2';
-import Chat3 from './pages/chat/chat3';
-import ChannelChat from './components/chatComponents/ChannelChat';
+import { Routes, Route } from "react-router-dom";
+import {
+  Chat,
+  DentalLab,
+  DentalStore,
+  Dentist,
+  Intro,
+  Login,
+  Signup,
+  SuccessPage,
+} from "./pages";
+import Chat1 from "./pages/chat/chat1";
+import Chat2 from "./pages/chat/chat2";
+import Chat3 from "./pages/chat/chat3";
+import ChannelChat from "./components/chatComponents/ChannelChat";
 export default function App() {
   return (
     <Routes>
+      <Route path="/success" element={<SuccessPage />} />
       <Route path="/" element={<Intro />} />
       <Route path="/dentist" element={<Dentist />} />
       <Route path="/dental-lab" element={<DentalLab />} />
@@ -19,7 +29,8 @@ export default function App() {
         <Route path="chat1" element={<Chat1 />} />
         <Route path="chat2" element={<Chat2 />} />
         <Route path="chat3" element={<Chat3 />} />
-         <Route path=":serverId/:channelId" element={<ChannelChat />} /> {/* Route for channels */}
+        <Route path=":serverId/:channelId" element={<ChannelChat />} />{" "}
+        {/* Route for channels */}
       </Route>
     </Routes>
   );
---
+++ server/services/auth.services.js
@@ -0,0 +1,48 @@
+import bcrypt from "bcrypt";
+import User from "../models/User.js";
+import { ApiError } from "../utils/ApiError.js";
+
+export const createUser = async (data) => {
+  const {
+    firstName,
+    lastName,
+    proofOfProfession,
+    email,
+    password: pass,
+    role,
+  } = data;
+
+  console.log(data);
+
+  // Check if proofOfProfession is provided
+  if (!proofOfProfession) {
+    throw new ApiError(400, "Proof of Profession is required");
+  }
+
+  // Check if the user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    throw new ApiError(400, "User already exists");
+  }
+
+  // Ensure only one admin exists
+  if (role === "admin") {
+    throw new ApiError(400, "This role is not allowed");
+  }
+
+  // Hash the password before saving the user
+  const hashedPassword = await bcrypt.hash(pass, 10);
+
+  // Create new user
+  const user = await User.create({
+    firstName,
+    lastName,
+    proofOfProfession,
+    email,
+    password: hashedPassword,
+    role,
+  });
+
+  const { password, ...safeUser } = user._doc;
+  return safeUser;
+};
